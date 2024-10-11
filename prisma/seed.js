64adac3af417a17aee970a477c5a728e5b8491c7
Aliyah M Smith <aliyahms@umich.edu>
2024-10-11 01:39:36 -0400

    Defined the Employee model in schema and created seed file to seed the database with 10 initial employees

---
+++ prisma/seed.js
@@ -0,0 +1,14 @@
+const prisma = require("../prisma");
+const seed = async () => {
+  // Create 10 employees with placeholder titles
+  await prisma.employee.createMany({
+    data: Array.from({ length: 10 }, (_, i) => ({ name: "Employee ${i}" })),
+  });
+};
+seed()
+  .then(async () => await prisma.$disconnect())
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
